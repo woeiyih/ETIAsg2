2a34ff27c47beb0b8385079051d7455d86271f23
woeiyih <74339370+woeiyih@users.noreply.github.com>
2022-02-06 18:20:14 +0800

    manual upload latest commit
    error with git in vs code

--- main.go
+++ main.go
@@ -9,24 +9,28 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 //For Student's Microservice
+//var students map[string]Students
 var students Students
 var StudentID string
 var db *sql.DB
 
 type Students struct {
 	StudentID   string
-	Name        string
-	Description string
+	StudentName string
+	DOB         string
+	Address     string
+	PhoneNumber string
 }
 
 func CreateNewStudent(db *sql.DB, s Students) {
 
-	query := fmt.Sprintf("INSERT INTO Students VALUES ('%s', '%s', '%s')",
-		s.StudentID, s.Name, s.Description)
+	query := fmt.Sprintf("INSERT INTO Students VALUES ('%s', '%s', '%s', '%s', '%s')",
+		s.StudentID, s.StudentName, s.DOB, s.Address, s.PhoneNumber)
 
 	_, err := db.Query(query)
 
@@ -36,8 +40,8 @@ func CreateNewStudent(db *sql.DB, s Students) {
 }
 
 func UpdateStudent(db *sql.DB, s Students) {
-	query := fmt.Sprintf("UPDATE Students SET Name='%s', Description='%s' WHERE StudentID='%s'",
-		s.Name, s.Description, s.StudentID)
+	query := fmt.Sprintf("UPDATE Students SET StudentName='%s', DOB='%s', Address='%s', s.PhoneNumber='%s' WHERE StudentID='%s'",
+		s.StudentName, s.DOB, s.Address, s.PhoneNumber, s.StudentID)
 	_, err := db.Query(query)
 	if err != nil {
 		panic(err.Error())
@@ -46,7 +50,7 @@ func UpdateStudent(db *sql.DB, s Students) {
 
 func ViewStudent(db *sql.DB, s Students) {
 	query := fmt.Sprintf("SELECT * FROM Students WHERE StudentID='%s'",
-		s.StudentID, s.Name, s.Description)
+		s.StudentID)
 	_, err := db.Query(query)
 	if err != nil {
 		panic(err.Error())
@@ -61,28 +65,72 @@ func DeleteStudent(db *sql.DB, s Students) {
 		panic(err.Error())
 	}
 }
-func ListStudents(db *sql.DB, s Students) {
+func ListStudents(db *sql.DB) []Students {
+	/*if r.Method == "GET" {
+		if _, ok := students[params["StudentID"]]; ok {
+			json.NewEncoder(w).Encode(
+				students[params["StudentID"]])
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No student found"))
+		}
+
+	}*/
+	results, err := db.Query("SELECT StudentID, StudentName FROM Tutor")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	var students []Students
+	for results.Next() {
+		var getStudent Students
+		err = results.Scan(&getStudent.StudentID, &getStudent.StudentName)
+		if err != nil {
+
+			panic(err.Error())
+		}
+		students = append(students, getStudent)
+	}
+	return students
+
+}
+
+/* func ListStudents(db *sql.DB, s Students) {
 	query := fmt.Sprintf("SELECT * FROM Students",
-		s.StudentID, s.Name, s.Description)
+		s.StudentID, s.StudentName, s.DOB, s.Address, s.PhoneNumber)
 	_, err := db.Query(query)
 	if err != nil {
 		panic(err.Error())
 	}
 }
+*/
 
 func SearchStudents(db *sql.DB, s Students) {
 	query := fmt.Sprintf("SELECT * FROM Students WHERE Name='%s'",
-		s.StudentID, s.Name, s.Description)
+		s.StudentName)
 	_, err := db.Query(query)
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
-func student(w http.ResponseWriter, r *http.Request) {
+/*func searchstudent(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		if _, ok := students[params["StudentID"]]; ok {
+			json.NewEncoder(w).Encode(
+				students[params["StudentID"]])
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No student found"))
+		}
 
+	}
+}*/
+
+func student(w http.ResponseWriter, r *http.Request) {
+	////params := mux.Vars(r)
 	if r.Header.Get("Content-type") == "application/json" {
-		// POST is for creating new passenger
+		// POST is for creating new student
 		if r.Method == "POST" {
 			// read the string sent to the service
 			var newStudent Students
@@ -91,110 +139,99 @@ func student(w http.ResponseWriter, r *http.Request) {
 			if err == nil {
 				// convert JSON to object
 				json.Unmarshal(reqBody, &newStudent)
-				//Check if user fill up the required information for registering Passenger's account
-				if newStudent.StudentID == "" || newStudent.Name == "" || newStudent.Description == "" {
+				//Check user has filled up the required information for registering
+				if newStudent.StudentID == "" || newStudent.StudentName == "" || newStudent.DOB == "" || newStudent.Address == "" || newStudent.PhoneNumber == "" {
 					w.WriteHeader(http.StatusUnprocessableEntity)
-					w.Write([]byte("422 - Please supply passenger " + "information " + "in JSON format"))
+					w.Write([]byte("422 - Please supply student " + "information " + "in JSON format"))
 					return
 				} else {
-					CreateNewStudent(db, newStudent) //Once everything is checked, passenger's account will be created
+					CreateNewStudent(db, newStudent) //Validation before creating
 					w.WriteHeader(http.StatusCreated)
-					w.Write([]byte("201 - Successfully created passenger's account"))
+					w.Write([]byte("201 - Successfully created student account"))
 				}
 			} else {
 				w.WriteHeader(
 					http.StatusUnprocessableEntity)
-				w.Write([]byte("422 - Please supply passenger information " +
+				w.Write([]byte("422 - Please supply student information " +
 					"in JSON format"))
 			}
 		}
-		//---PUT is for creating or updating existing passenger---
+		//PUT - create/update existing student
 		if r.Method == "PUT" {
-			queryParams := r.URL.Query() //used to resolve the conflict of calling API using the '%s'?PassengerID='%s' method
+			queryParams := r.URL.Query()
 			StudentID = queryParams["StudentID"][0]
 			reqBody, err := ioutil.ReadAll(r.Body)
 			if err == nil {
 				json.Unmarshal(reqBody, &students)
-				//Check if user fill up the required information for updating Passenger's account information
-				if students.StudentID == "" || students.Name == "" || students.Description == "" {
+				//Check if user fill up the required information for updating student account information
+				if students.StudentID == "" || students.StudentName == "" || students.DOB == "" || students.Address == "" || students.PhoneNumber == "" {
 					w.WriteHeader(http.StatusUnprocessableEntity)
-					w.Write([]byte("422 - Please supply passenger " + " information " + "in JSON format"))
+					w.Write([]byte("422 - Please supply `student` " + " information " + "in JSON format"))
 				} else {
 					students.StudentID = StudentID
 					UpdateStudent(db, students)
 					w.WriteHeader(http.StatusAccepted)
-					w.Write([]byte("202 - Successfully updated passenger's information"))
+					w.Write([]byte("202 - Successfully updated student information"))
 				}
 			} else {
 				w.WriteHeader(
 					http.StatusUnprocessableEntity)
-				w.Write([]byte("422 - Please supply " + "passenger information " + "in JSON format"))
+				w.Write([]byte("422 - Please supply " + "student information " + "in JSON format"))
 			}
 		}
 
 	}
 	if r.Method == "GET" {
-		if _, ok := students[params["StudentID"]]; ok {
+		/*if _, ok := students[params["StudentID"]]; ok {
 			json.NewEncoder(w).Encode(
 				students[params["StudentID"]])
 		} else {
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte("404 - No student found"))
-		}
+		}*/
+		StudentID := r.URL.Query().Get("StudentID")
+		fmt.Println("StudentID: ", StudentID)
+		students := ListStudents(db)
+
+		json.NewEncoder(w).Encode(&students)
 
 	}
-	//---Deny any deletion of passenger's account or other passenger's information
+	//---Deny any deletion of student's account or other student's information
 	if r.Method == "DELETE" {
 		w.WriteHeader(http.StatusForbidden)
-		w.Write([]byte("403 - For audit purposes, passenger's account cannot be deleted."))
-	}
-}
-
-func liststudent(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		if _, ok := students[params["StudentID"]]; ok {
-			json.NewEncoder(w).Encode(
-				students[params["StudentID"]])
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 - No student found"))
-		}
-
-	}
-}
-
-func searchstudent(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		if _, ok := students[params["StudentID"]]; ok {
-			json.NewEncoder(w).Encode(
-				students[params["StudentID"]])
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 - No student found"))
-		}
-
+		w.Write([]byte("403 - For audit purposes, student account cannot be deleted."))
 	}
 }
 
 func main() {
-	// instantiate passengers
-	ridesharing_db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/ridesharing_db")
+	// instantiate students
+	a2_db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/a2_db")
 
-	db = ridesharing_db
+	db = a2_db
 	// handle error
 	if err != nil {
 		panic(err.Error())
 	}
-	//handle the API connection across all three microservices, Passengers, Trips and Drivers
+	//handle the API connection
 	router := mux.NewRouter()
-	router.HandleFunc("/students", student).Methods(
+
+	router.HandleFunc("/students", students).Methods("GET", "POST", "PUT", "DELETE")
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+
+	fmt.Println("Student Microservice API --> Listening at port 8011")
+	log.Fatal(http.ListenAndServe(":8011", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+
+	/*router.HandleFunc("/students", student).Methods(
 		"GET", "POST", "PUT", "DELETE")
-	router.HandleFunc("/students/liststudents", liststudent).Methods(
+	router.HandleFunc("/students/liststudents", ListStudents()).Methods(
 		"GET")
-	router.HandleFunc("/students/searchstudents", searchstudent).Methods(
+	router.HandleFunc("/students/searchstudents", ListStudents()).Methods(
 		"GET")
 	fmt.Println("Students microservice API --> Listening at port 5001")
-	log.Fatal(http.ListenAndServe(":5001", router))
+	log.Fatal(http.ListenAndServe(":5001", router))*/
 
 	defer db.Close()
 }
